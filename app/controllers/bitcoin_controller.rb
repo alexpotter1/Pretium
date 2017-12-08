6bfe80b8184fb98f1ffd888b268c6ef0df251151
Alex Potter <alexpotter1@icloud.com>
2017-12-08 18:32:35 +0000

    update testing of cryptocompare api

--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -5,7 +5,6 @@ class BitcoinController < ApplicationController
   def index
     cryptoPriceObject = Cryptocompare::Price.find('BTC', 'GBP')
     @price = cryptoPriceObject['BTC']['GBP']
-
   end
 
 end
