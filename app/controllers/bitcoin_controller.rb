d7a2e03ac438819592c3bb479709a2f9e2298163
Alex Potter <alexpotter1@icloud.com>
2017-12-08 17:21:27 +0000

    cryptocompare: API works...

--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -1,8 +1,11 @@
-require 'coinbase/exchange'
+require 'cryptocompare'
 
 class BitcoinController < ApplicationController
 
   def index
+    cryptoPriceObject = Cryptocompare::Price.find('BTC', 'GBP')
+    @price = cryptoPriceObject['BTC']['GBP']
+
   end
 
 end
