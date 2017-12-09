44a8efc97cfd53a2022044b7f93280faeaa88c77
Alex Potter <alexpotter1@icloud.com>
2017-12-09 16:52:06 +0000

    Currency: Add currency type field

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,10 @@
 class Currency < ApplicationRecord
+
+  validates :currency_type, presence: true
   has_many :prices
+
+  private
+    def currency_params
+      params.require(:currencies).require(:currency_type)
+    end
 end
---
+++ db/migrate/20171209165136_add_currency_type_to_currencies.rb
@@ -0,0 +1,5 @@
+class AddCurrencyTypeToCurrencies < ActiveRecord::Migration[5.1]
+  def change
+    add_column :currencies, :currency_type, :string
+  end
+end
