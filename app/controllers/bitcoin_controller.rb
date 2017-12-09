534ad22b2ea4a2575255342e465de3610d22217d
Alex Potter <alexpotter1@icloud.com>
2017-12-09 19:24:10 +0000

    fix getting price data from cryptocompare

--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -30,14 +30,16 @@ class BitcoinController < ApplicationController
 
   def processCryptoPriceObject(crypto_price_object, currency_object)
     # only save if response is HTTP OK (200)
-    if crypto_price_object['response'] == "Success"
-      crypto_price_object['data'].each do | price |
+    if crypto_price_object['Response'] == "Success"
+      crypto_price_object['Data'].each do | price |
         # only take the closing price for each time interval, for now
         price_value = price['close']
 
         # time is a Unix timestamp
         price_time = price['time']
         @currency.prices.create(:price => price_value, :time => price_time)
+        puts 'attempted to create price'
+        puts @currency.prices.first
       end
       @currency.prices.save!
       @currency.save!
@@ -48,6 +50,7 @@ class BitcoinController < ApplicationController
   def updateBtcPrices
     # Obtain the time interval from Preference
     @crypto_prices = nil
+    puts 'updating prices now'
     unless @prefs.nil?
       tinterval = @prefs.time_interval
       if tinterval == "1m"
@@ -55,6 +58,7 @@ class BitcoinController < ApplicationController
         self.processCryptoPriceObject(@crypto_prices, @currency)
       elsif tinterval == "1h"
         @crypto_prices = Cryptocompare::HistoHour.find(@currency.currency_type, @default_fiat)
+        puts 'calling pcpo'
         self.processCryptoPriceObject(@crypto_prices, @currency)
       elsif tinterval == "1d"
         @crypto_prices = Cryptocompare::HistoDay.find(@currency.currency_type, @default_fiat)
--- db/migrate/20171208200103_create_bitcoin_prices.rb
+++ db/migrate/20171208200103_create_bitcoin_prices.rb
@@ -1,7 +1,7 @@
 class CreateBitcoinPrices < ActiveRecord::Migration[5.1]
   def change
     create_table :bitcoin_prices do |t|
-
+      t.belongs_to :currency
       t.timestamps
     end
   end
