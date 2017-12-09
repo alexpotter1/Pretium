688050be70a9b91eefa2558b40a566e8c046c8ec
Alex Potter <alexpotter1@icloud.com>
2017-12-09 16:12:46 +0000

    BitcoinController: begin controlling the Bitcoin model

--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -8,20 +8,35 @@ class BitcoinController < ApplicationController
     @default_fiat = 'GBP'
     @crypto = 'BTC'
     @prefs = nil
+    @btc = nil
 
     if Preference.where(id: 1).empty?
       @prefs = Preference.create(:fiat_currency => @default_fiat)
       @prefs.save
     else
-      self.obtainModelsFromDB
+      self.obtainPrefModelFromDB
     end
+
+    if Bitcoin.where(id: 1).empty?
+      @btc = Bitcoin.create
+      @btc.save
+    else
   end
 
-  def obtainModelsFromDB
+  def updateBtcPrices
+    
+  end
+
+  def obtainPrefModelFromDB
     @prefs = Preference.first
   end
 
+  def obtainBtcModelFromDB
+    @btc = Bitcoin.first
+  end
+
   def index
+    # update prices from cryptocompare, store in model
   end
 
 end
