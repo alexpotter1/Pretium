8627c5960a8a2fb4d9fdb0b3ad7518e46bfac8c6
Alex Potter <alexpotter1@icloud.com>
2017-12-10 22:02:54 +0000

    Update .new() call to CurrencyController to reference correct args

--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -3,8 +3,8 @@ class BitcoinController < ApplicationController
   def initialize
     super
     @default_fiat = 'GBP'
-    @currency = nil
     @prefs_controller = PreferencesController.new
+    @currency_controller = CurrencyController.new(currency_type: 'BTC', @prefs_controller)
   end
 
   def index
--- app/controllers/ethereum_controller.rb
+++ app/controllers/ethereum_controller.rb
@@ -3,8 +3,8 @@ class EthereumController < ApplicationController
   def initialize
     super
     @default_fiat = 'GBP'
-    @currency_controller = CurrencyController.new(currency_type: 'ETH')
     @prefs_controller = PreferencesController.new
+    @currency_controller = CurrencyController.new(currency_type: 'ETH', @prefs_controller)
   end
 
   def index
