9fdca8676f178de74d4b07f24106a4eb3efff694
Alex Potter <alexpotter1@icloud.com>
2017-12-11 14:01:21 +0000

    CurrencyTest: add model test

--- test/models/currency_test.rb
+++ test/models/currency_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CurrencyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @ctype = 'BTC'
+    @currency = Currency.create(:currency_type => @ctype)
+  end
+
+  test 'test accessor currency type successful' do
+    assert_equal @currency.currency_type, @ctype
+  end
 end
